33bbd805fac2ab657625447d415c573ecc9a68c3
Ryan McGeary <ryanongit@mcgeary.org>
2008-11-19 10:32:41 -0500

    Improved twitter substitution support in About section

--- application.js
+++ application.js
@@ -38,8 +38,10 @@ Number.prototype.px = function(){ return this.toString() + "px" }
 $(function(){
 
   $("a[name=about]").click(function(){ $("#about").toggle() })
-  var about = $("#about p:first").text().replace(/(@([a-zA-Z0-9]+))/g, '<a href="http://twitter.com/$2">$1</a>')
-  $("#about p:first").html(about)
+
+  $("#about p").each(function() {
+    $(this).html($(this).text().replace(/(@([a-zA-Z0-9]+))/g, '<a href="http://twitter.com/$2">$1</a>'))
+  })
 
   var searchString = $.getQueryString({ id: "q" })
   var inputField   = $("input[type=text]")
