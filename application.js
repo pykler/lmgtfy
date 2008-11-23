a67a1e461fa757745b7d1463086ab26460bbef24
Jim Garvin <jim@thegarvin.com>
2008-11-23 15:34:54 -0500

    i18n support added. spanish translation included.

--- application.js
+++ application.js
@@ -32,17 +32,121 @@
   };
 })(jQuery);
 
+// http://keith-wood.name/localisation.html
+// Localisation assistance for jQuery v1.0.2.
+// Written by Keith Wood (kbwood@iprimus.com.au) June 2007. 
+// Dual licensed under the GPL (http://dev.jquery.com/browser/trunk/jquery/GPL-LICENSE.txt) and 
+// MIT (http://dev.jquery.com/browser/trunk/jquery/MIT-LICENSE.txt) licenses. 
+// Please attribute the author if you use it.
+(function($) {
+
+  // Load applicable localisation package(s) for one or more jQuery packages.
+  // Assumes that the localisations are named <base>-<lang>.js
+  // and loads them in order from least to most specific.
+  // For example, $.localise('jquery-calendar');
+  // with the browser set to 'en-US' would attempt to load
+  // jquery-calendar-en.js and jquery-calendar-en-US.js.
+  // Also accepts an array of package names to process.
+  // Optionally specify whether or not to include the base file,
+  // the desired language, and/or the timeout period, e.g.
+  // $.localise(['jquery-calendar', 'jquery-timeentry'], 
+  //      {loadBase: true; language: 'en-AU', timeout: 300});
+  $.localise = function(pkg, settings) {
+    settings = settings || {}
+    var saveSettings = {async: $.ajaxSettings.async, timeout: $.ajaxSettings.timeout};
+    $.ajaxSetup({async: false, timeout: (settings && settings.timeout ? settings.timeout : 500)});
+    
+    var intermediateLangData = {}
+    function loadLanguage(pkg, lang, level) {
+      level = level || 1;
+      if (settings && settings.loadBase && level == 1) {
+        intermediateLangData = {};
+        var file = pkg + '.json';
+        jsonCall(file, pkg, lang, level);
+      }
+      else if (level == 1) {
+        intermediateLangData = {};
+        loadLanguage(pkg, lang, 2);
+      }
+      else if (level == 2 && lang.length >= 2) {
+        var file = pkg + '-' + lang.substring(0, 2) + '.json';
+        jsonCall(file, pkg, lang, level)
+      }
+      else if (level == 3 && lang.length >= 5) {
+        var file = pkg + '-' + lang.substring(0, 5) + '.json';
+        jsonCall(file, pkg, lang, level)
+      }
+    }
+    
+    function jsonCall(file, pkg, lang, level) {
+      if (settings.pathPrefix) file = settings.pathPrefix + "/" + file
+      $.getJSON(file, null, function(d){
+        $.extend(intermediateLangData, d);
+        notifyDelegateLanguageLoaded(intermediateLangData);
+        loadLanguage(pkg, lang, level + 1);
+      });
+    }
+    
+    function notifyDelegateLanguageLoaded(data) {
+      if (settings.callback) settings.callback(data)
+    }
+    
+    var lang = normaliseLang(settings && settings.language ? settings.language : $.defaultLanguage);
+
+    if (settings.skipLanguage && settings.skipLanguage == lang) return
+    loadLanguage(pkg, lang, 1)
+    
+    $.ajaxSetup(saveSettings);
+  };
+  
+  // Retrieve the default language set for the browser.
+  $.defaultLanguage = normaliseLang(navigator.language 
+    ? navigator.language       // Mozilla 
+    : navigator.userLanguage   // IE
+  );
+  
+  // Ensure language code is in the format aa-AA. 
+  function normaliseLang(lang) {
+   lang = lang.replace(/_/, '-').toLowerCase();
+   if (lang.length > 3) {
+     lang = lang.substring(0, 3) + lang.substring(3).toUpperCase();
+   }
+   return lang;
+  }
+
+  // Determine whether an object is an array. 
+  function isArray(a) {
+   return (a.constructor && a.constructor.toString().match(/\Array\(\)/));
+  }
+})(jQuery);
+
 // helpers
 Number.prototype.px = function(){ return this.toString() + "px" }
 
-function gentlyEncode(string){
+function gentlyEncode(string) {
   return ( encodeURIComponent
            ? encodeURIComponent(string).replace(/%20(\D)?/g, "+$1").replace(/'/g, escape("'"))
            : escape(string).replace(/\+/g, "%2B").replace(/%20/g, "+") )
 }
 
-function gentlyDecode(string){
+function gentlyDecode(string) {
   return decodeURIComponent ? decodeURIComponent(string) : unescape(string)
+}
+
+// default lang necessities
+var LMGTFY = {}
+LMGTFY.lang = {
+  setup: {
+    type_question: "Type a question, click a button.",
+    share_link:    "Share the link below.",
+    or:            "or"
+  },
+  
+  play: {
+    step_1: "Step 1: Type in your question",
+    step_2: "Step 2: Click the Search button",
+    pwnage: "Was that so hard?"
+  }
 }
 
 // app code
@@ -58,28 +162,55 @@ $(function(){
   if (searchString && searchString.length > 0) googleItForThem()
   else getTheSearchTerms()
 
-  function initializeContent(){
+  function initializeContent() {
     $("a[name=about]").click(function(){ $("#about").toggle(); return false; })
-
     $('input.copyable').click(function() { $(this).select(); });
-
+    linkifyAbout()
+    
+    var localise_opts = { pathPrefix: 'lang', callback: languageLoaded, skipLanguage: "en-US" }
+    var lang = $.getQueryString({ id: "lang" })
+    if (lang) localise_opts.language = lang
+    $.localise('lmgtfy', localise_opts);
+  }
+  
+  function languageLoaded(data) {
+    LMGTFY.lang = data
+    var keys, value
+    $("[rel*=localize]").each(function(){
+      elem = $(this)
+      keys = elem.attr("rel").split(/\./)
+      value = keys.length == 2 ? data[keys[1]] : data[keys[1]][keys[2]]
+      if (elem.attr('tagName') == "INPUT")
+        elem.val(value)
+      else
+        elem.text(value)
+    })
+    linkifyAbout()
+  }
+  
+  function linkifyAbout() {
     $("#about p").each(function() {
       $(this).html($(this).text().replace(/(@([a-zA-Z0-9]+))/g, '<a href="http://twitter.com/$2">$1</a>'))
     })
   }
 
-  function instruct(words){ instructions.html(words) }
+  function instruct(langkey) {
+    var keys = langkey.split(/\./)
+    var value = keys.length == 1 ? LMGTFY.lang[keys[0]] : LMGTFY.lang[keys[0]][keys[1]]
+    instructions.html(value) 
+  }
 
-  function getTheSearchTerms(){
+  function getTheSearchTerms() {
     $("form").submit(function(){ $("#search").click(); return false; })
-    instruct("Type a question, click a button.")
+    instruct("setup.type_question")
     inputField.focus().select()
 
     $("input[type=button]").click(function(e){
-      instruct("Share a link below.")
+      instruct("setup.share_link")
 
       var l   = window.location
-      var url = l.protocol + "//" + l.hostname + l.pathname + "?"
+      var port = l.port == "80" ? "" : ":" + l.port
+      var url = l.protocol + "//" + l.hostname + port + l.pathname + "?"
 
       strings = [ "q=" + gentlyEncode(inputField.val()) ]
       if (this.id == "lucky") strings.push("l=1")
@@ -96,10 +227,10 @@ $(function(){
     })
   }
 
-  function googleItForThem(){
+  function googleItForThem() {
     $("body").css("cursor", "wait")
     fakeMouse.show()
-    instruct("Step 1: Type in your question")
+    instruct("play.step_1")
 
     fakeMouse.animate({
       top:  (inputField.position().top  + 15).px(),
@@ -122,12 +253,12 @@ $(function(){
     }
 
     function doneTyping(){
-      instruct("Step 2: Click the Search button")
+      instruct("play.step_2")
       fakeMouse.animate({
         top:  (button.position().top  + 10).px(),
         left: (button.position().left + 30).px()
       }, 2000, 'swing', function(){
-        instruct("Was that so hard?")
+        instruct("play.pwnage")
         button.focus()
         setTimeout(redirect, 2000)
       })
