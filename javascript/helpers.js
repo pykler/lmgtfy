bdc5096f1b20394683c867786b6cf2200b1a78bf
Ryan McGeary <ryanongit@mcgeary.org>
2009-02-01 23:28:37 -0500

    Cleanup and Fixed sponsor clicks so that they don't duplicate tracking during ad rotation

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,49 +1,44 @@
-(function($) {
-  $.sendToClipboard = function(text) {
-    var copier = $("#flash_copier");
-    if (copier.size() == 0) {
-      copier = $('<div id="flash_copier"></div>').appendTo("body");
-    }
-    copier.html('<embed src="_clipboard.swf" FlashVars="clipboard='+encodeURIComponent(text)+'" width="0" height="0" type="application/x-shockwave-flash"></embed>');
-  };
-})(jQuery);
-
-(function($) {
-  $.fn.centerOver = function(element, topOffset, leftOffset) {
-    topOffset = topOffset || 0;
-    leftOffset = leftOffset || 0;
-    var self = this;
-    self.css({
-      top: (element.position().top + element.outerHeight()/2 - self.height()/2 + topOffset).px(),
-      left: (element.position().left + element.outerWidth()/2 - self.width()/2 + leftOffset).px()
-    });
-    return self;
-  };
-})(jQuery);
-
-(function($) {
-  $.fn.sponsor = function(programFile, callback) {
-    var self = this;
-    $.getJSON(programFile, function(program) {
-      var sponsor = program.slots[rand(program.slots.length)];
-      var id = sponsor.id;
-      var anchor = self.find("a");
-      anchor.attr("href", sponsor.url);
-      anchor.find("img").attr("src", sponsor.image);
-      anchor.find("p").html(sponsor.message);
-      if (pageTracker) {
-        pageTracker._trackPageview("/sponsor/" + id);
-        anchor.click(function() { pageTracker._trackPageview("/outgoing/sponsor/" + id); });
-      }
-      if (callback) callback.call(self);
-    });
-    return self;
-  };
-
-  function rand(max) {
-    return Math.floor(Math.random() * max);
+$.sendToClipboard = function(text) {
+  var copier = $("#flash_copier");
+  if (copier.size() == 0) {
+    copier = $('<div id="flash_copier"></div>').appendTo("body");
   }
-})(jQuery);
+  copier.html('<embed src="_clipboard.swf" FlashVars="clipboard='+encodeURIComponent(text)+'" width="0" height="0" type="application/x-shockwave-flash"></embed>');
+};
+
+$.fn.centerOver = function(element, topOffset, leftOffset) {
+  topOffset = topOffset || 0;
+  leftOffset = leftOffset || 0;
+  var self = this;
+  self.css({
+    top: (element.position().top + element.outerHeight()/2 - self.height()/2 + topOffset).px(),
+    left: (element.position().left + element.outerWidth()/2 - self.width()/2 + leftOffset).px()
+  });
+  return self;
+};
+
+$.fn.sponsor = function(programFile, callback) {
+  var self = this;
+  $.getJSON(programFile, function(program) {
+    var sponsor = program.slots[rand(program.slots.length)];
+    var id = sponsor.id;
+    var anchor = self.find("a");
+    anchor.attr("href", sponsor.url);
+    anchor.find("img").attr("src", sponsor.image);
+    anchor.find("p").html(sponsor.message);
+    if (pageTracker) {
+      pageTracker._trackPageview("/sponsor/" + id);
+      anchor.unbind("click");
+      anchor.click(function() { pageTracker._trackPageview("/outgoing/sponsor/" + id); });
+    }
+    if (callback) callback.call(self);
+  });
+  return self;
+};
+
+function rand(max) {
+  return Math.floor(Math.random() * max);
+}
 
 Number.prototype.px = function(){ return this.toString() + "px"; };
 
