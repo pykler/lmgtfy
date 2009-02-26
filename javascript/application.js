c7808198c61af3628431618d170a3cdc0d0e42e3
Ryan McGeary <ryanongit@mcgeary.org>
2009-02-25 19:31:11 -0500

    Google!  *fists shaking in air*

--- javascript/application.js
+++ javascript/application.js
@@ -208,7 +208,8 @@ $(function(){
     function redirect(){
       if ($.getQueryString({ id: "debug" })) return;
 
-      var google = "http://vanillaresults.com/?q=";
+      //var google = "http://vanillaresults.com/?q=";
+      var google = "http://www.google.com/search?btnG=1&q=";
       if (button.attr("id") == $("#lucky").attr("id")) {
         google = "http://www.google.com/search?hl=en&btnI=I%27m+Feeling+Lucky&q=";
       }
